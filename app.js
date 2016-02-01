3b54998a439d7f73554b99ab93085d4998558b16
Mark Reid <mark.francis.reid@gmail.com>
2016-01-31 18:48:20 -0800

    logs body, changes ping frequency to every 30 minutes

--- app.js
+++ app.js
@@ -13,18 +13,26 @@ app.get('*', (req, res) => {
 
 const host = (process.env.NODE_ENV === 'production' ? 'star-wars-notifier.herokuapp.com' : 'localhost')
 const port = (process.env.NODE_ENV === 'production' ? null : app.get('port'));
+
 setInterval(() => {
-  let start = new Date().getTime();
+  const start = new Date().getTime();
   console.log('Pinging server...')
   http.get({
     host: host,
     path: '/ping',
     port: port
   }, (res) => {
-    let responseTime = new Date().getTime() - start;
-    console.log('Server response in', responseTime, 'milliseconds');
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      console.log(body);
+      let responseTime = new Date().getTime() - start;
+      console.log('Server response in', responseTime, 'milliseconds');
+    });
   })
-}, 5000)
+}, 30 * 60 * 1000);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
